e9de6fc0d0a895dc3d649b399d0e0353be51e2d1
unknown <haroldkengne@gmail.com>
2025-12-27 15:44:55 +0100

    implementation of application layer for user

---
+++ auth-service/src/main/java/fr/harxen/auth_service/application/command/RegisterUserCommand.java
@@ -0,0 +1,3 @@
+package fr.harxen.auth_service.application.command;
+
+public record RegisterUserCommand(String email, String password){}
---
+++ auth-service/src/main/java/fr/harxen/auth_service/application/usecase/RegisterUserUseCase.java
@@ -0,0 +1,36 @@
+package fr.harxen.auth_service.application.usecase;
+
+import fr.harxen.auth_service.application.command.RegisterUserCommand;
+import fr.harxen.auth_service.application.exception.EmailAlreadyUsedException;
+import fr.harxen.auth_service.application.result.RegisterUserResult;
+import fr.harxen.auth_service.domain.model.User;
+import fr.harxen.auth_service.domain.model.valueobject.Email;
+import fr.harxen.auth_service.domain.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+public class RegisterUserUseCase {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public RegisterUserUseCase(UserRepository userRepository, PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public RegisterUserResult execute(RegisterUserCommand command){
+        Email email = Email.of(command.email());
+
+        if (userRepository.findByEmail(email).isPresent()){
+            throw new EmailAlreadyUsedException();
+        }
+
+        String hashed = passwordEncoder.encode(command.password());
+        User user = User.createNew(email, hashed);
+        User saved = userRepository.save(user);
+
+        return new RegisterUserResult(saved.getId(), saved.getEmail().value());
+    }
+}
